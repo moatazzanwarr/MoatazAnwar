4bdbd958608f9955053dec5b01892391892e9f7d
moatazzanwarr <moatazzanwarr@gmil.com>
2024-05-06 22:24:24 +0300

    6-5-2024

--- js/main.js
+++ js/main.js
@@ -5,3 +5,37 @@ let menue_bar = document.querySelector("#menu_bar");
 menue_bar.addEventListener("click", () =>{
     mobile_nav.classList.toggle("flex");
 })
+
+// Loading Animatin
+let loading = document.querySelector('#loading');
+setTimeout(() => {
+    loading.style.display = "none";
+}, 3000)
+
+
+// Scrolling Animation
+
+window.addEventListener('scroll', () => {
+    const element = document.querySelector(".animate");
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    if(window.innerWidth < 1100){
+        if (scrollPosition > 3150){
+            element.style.transform = "translateY(0)";
+            element.style.opacity = "1";
+        }else{
+            element.style.transform = "translateY(300px)";
+            element.style.opacity = "0";
+    
+        }
+    }else if(window.innerWidth > 1100){
+        if (scrollPosition > 1850){
+            element.style.transform = "translateY(0)";
+            element.style.opacity = "1";
+        }else{
+            element.style.transform = "translateY(300px)";
+            element.style.opacity = "0";
+    
+        }
+    }
+})
