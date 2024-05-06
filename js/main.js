2939c57deab9b0e810d04d97235bd5e3829a92de
moatazzanwarr <moatazzanwarr@gmil.com>
2024-05-06 23:07:05 +0300

    1

--- js/main.js
+++ js/main.js
@@ -15,27 +15,112 @@ setTimeout(() => {
 
 // Scrolling Animation
 
+
+
 window.addEventListener('scroll', () => {
-    const element = document.querySelector(".animate");
+    const element_1 = document.querySelector(".about-all");
     const scrollPosition = window.scrollY;
     console.log(scrollPosition);
     if(window.innerWidth < 1100){
-        if (scrollPosition > 3150){
-            element.style.transform = "translateY(0)";
-            element.style.opacity = "1";
+        if (scrollPosition > 460){
+            element_1.style.transform = "translateY(0)";
+            element_1.style.opacity = "1";
         }else{
-            element.style.transform = "translateY(300px)";
-            element.style.opacity = "0";
+            element_1.style.transform = "translateY(900px)";
+            element_1.style.opacity = "0";
     
         }
     }else if(window.innerWidth > 1100){
-        if (scrollPosition > 1850){
-            element.style.transform = "translateY(0)";
-            element.style.opacity = "1";
+        if (scrollPosition > 340){
+            element_1.style.transform = "translateY(0)";
+            element_1.style.opacity = "1";
         }else{
-            element.style.transform = "translateY(300px)";
-            element.style.opacity = "0";
+            element_1.style.transform = "translateY(900px)";
+            element_1.style.opacity = "0";
     
         }
+    }else{
+        return ''
+    }
+})
+
+window.addEventListener('scroll', () => {
+    const element_2 = document.querySelector(".services-all");
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    if(window.innerWidth < 1100){
+        if (scrollPosition > 1740){
+            element_2.style.transform = "translateY(0)";
+            element_2.style.opacity = "1";
+        }else{
+            element_2.style.transform = "translateY(900px)";
+            element_2.style.opacity = "0";
+    
+        }
+    }else if(window.innerWidth > 1100){
+        if (scrollPosition > 1040){
+            element_2.style.transform = "translateY(0)";
+            element_2.style.opacity = "1";
+        }else{
+            element_2.style.transform = "translateY(900px)";
+            element_2.style.opacity = "0";
+    
+        }
+    }else{
+        return ''
+    }
+})
+
+window.addEventListener('scroll', () => {
+    const element_3 = document.querySelector(".animate");
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    if(window.innerWidth < 1100){
+        if (scrollPosition > 3130){
+            element_3.style.transform = "translateY(0)";
+            element_3.style.opacity = "1";
+        }else{
+            element_3.style.transform = "translateY(900px)";
+            element_3.style.opacity = "0";
+    
+        }
+    }else if(window.innerWidth > 1100){
+        if (scrollPosition > 1770){
+            element_3.style.transform = "translateY(0)";
+            element_3.style.opacity = "1";
+        }else{
+            element_3.style.transform = "translateY(900px)";
+            element_3.style.opacity = "0";
+    
+        }
+    }else{
+        return ''
+    }
+})
+
+window.addEventListener('scroll', () => {
+    const element_4 = document.querySelector(".contact-all-pr");
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    if(window.innerWidth < 1100){
+        if (scrollPosition > 5550){
+            element_4.style.transform = "translateY(0)";
+            element_4.style.opacity = "1";
+        }else{
+            element_4.style.transform = "translateY(900px)";
+            element_4.style.opacity = "0";
+    
+        }
+    }else if(window.innerWidth > 1100){
+        if (scrollPosition > 2440){
+            element_4.style.transform = "translateY(0)";
+            element_4.style.opacity = "1";
+        }else{
+            element_4.style.transform = "translateY(900px)";
+            element_4.style.opacity = "0";
+    
+        }
+    }else{
+        return ''
     }
 })
